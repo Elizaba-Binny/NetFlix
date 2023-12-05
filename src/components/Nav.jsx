9f35c4b4a83a9932a5ba09f04310b6df88a9f4ca
53-Elizaba <elizaba.binny@gmail.com>
2023-12-05 22:22:46 +0530

    validation implemented

---
+++ src/components/Nav.jsx
@@ -0,0 +1,12 @@
+import './Nav.css'
+function Nav(){
+return(
+    <>
+    <div className="nav">
+        <img width={'160px'} src="https://upload.wikimedia.org/wikipedia/commons/7/7a/Logonetflix.png" alt="" />
+    </div>
+    </>
+)
+
+}
+export default Nav
